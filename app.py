fa4ac0cf7c3b389193d0bd4fc90b32c52661a7c7
jkstarling <beets09@yahoo.com>
2024-05-26 14:42:11 -0400

    26may24

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ st.write("""
         """)
  
 
-page_bg_img = '''
-<style>
-body {
-background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
-background-size: cover;
-}
-</style>
-'''
+# page_bg_img = '''
+# <style>
+# body {
+# background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+# background-size: cover;
+# }
+# </style>
+# '''
 
-st.markdown(page_bg_img, unsafe_allow_html=True)
+# st.markdown(page_bg_img, unsafe_allow_html=True)
