8a33d67b66d68f628618bf34d79c2ed13a3f06e8
jkstarling <beets09@yahoo.com>
2024-05-26 14:07:52 -0400

    26MAY24 - Udpates to Dashboard -includes Boxes w/colors

--- helper.py
+++ helper.py
@@ -5,43 +5,75 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-def arrow_format(val):
-    if val > 0:
-        return f':green[ (↑ {val:.1f}%)]'
+def arrow_form_perc(val):
+    epsilon = 0.001
+    if val > -epsilon and val < epsilon:
+        return '  (0%)'
+    elif val > 0:
+        return f':green[  (↑ {val:.2f}%)]'
     elif val < 0:
-        return f':red[ (↓ {abs(val):.1f}%)]'
-    elif val == np.nan():
-        return f'No data'
+        return f':red[  (↓ {abs(val):.2f}%)]'
     else:
-        return f' ({val:.0}%)'
+        return '  (No data)' 
 
-def kdollar_format(val):
+def arrow_form_num(val):
+    epsilon = 0.001
+    if val > -epsilon and val < epsilon:
+        return '  (0)'
+    elif val > 0:
+        return f':green[  (↑ {val:.2f})]'
+    elif val < 0:
+        return f':red[  (↓ {abs(val):.2f})]'
+    else:
+        return '  (No data)' 
+
+def pmix_form(val):
+    val = val * 100
+    if val >= 90:
+        return f':green[{val:.0f}%]'
+    else:
+        return f':red[{val:.0f}%]'
+
+def big5_form(val):
+    val = val * 100
+    if val >= 50:
+        return f':green[{val:.0f}%]'
+    else:
+        return f':red[{val:.0f}%]'
+
+def baytime_form(val):
+    if val <= 10:
+        return f':green[{val:.0f}]'
+    else:
+        return f':red[{val:.0f}]'
+
+def kdollar_form(val):
     return f'${val:,.2f}K'
 
-def dollar_format(val):
+def dollar_form(val):
     return f'${val:,.2f}'
 
-def perc_format(val):
+def perc_form(val):
     return f'{val:.2f}%'
 
-def numb_format(val):
+def numb_form(val):
     return f'{int(val):,}'
 
 def format_two_decimals(x):
     return f"{x:.2f}"
 
 def perc_print(mymetric, mynumb, myperc):
-    st.write(mymetric + arrow_format(myperc))    
-    st.markdown(f"""<p style='font-size:30px;'>{perc_format(mynumb)}</p>""", unsafe_allow_html=True)
+    st.write(mymetric + arrow_form(myperc))    
+    st.markdown(f"""<p style='font-size:30px;'>{perc_form(mynumb)}</p>""", unsafe_allow_html=True)
 
 def numb_print(mymetric, mynumb, myperc):
-    st.write(mymetric + arrow_format(myperc))
-    st.markdown(f"""<p style='font-size:30px;'>{numb_format(mynumb)}</p>""", unsafe_allow_html=True)
+    st.write(mymetric + arrow_form(myperc))
+    st.markdown(f"""<p style='font-size:30px;'>{numb_form(mynumb)}</p>""", unsafe_allow_html=True)
 
 def kdollar_print(mymetric, mynumb, myperc):
-    st.write(mymetric + arrow_format(myperc))
-    st.markdown(f"<p style='font-size:30px;'>{kdollar_format(mynumb)}</p>", unsafe_allow_html=True)
+    st.write(mymetric + arrow_form(myperc))
+    st.markdown(f"<p style='font-size:30px;'>{kdollar_form(mynumb)}</p>", unsafe_allow_html=True)
 
 def dollar_print(mymetric, mynumb, myperc):
-    st.write(mymetric + arrow_format(myperc))
-    st.markdown(f"<p style='font-size:30px;'>{dollar_format(mynumb)}</p>", unsafe_allow_html=True)
+    st.write(mymetric + arrow_form(myperc))
+    st.markdown(f"<p style='font-size:30px;'>{dollar_form(mynumb)}</p>", unsafe_allow_html=True)
--- pages/1_Take5_Oil.py
+++ pages/1_Take5_Oil.py
@@ -17,7 +17,10 @@ from helper import *
 from sklearn.linear_model import LinearRegression
 import hmac
 
-st.set_page_config(layout="wide")
+
+st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
+# st.set_page_config()
+
 
 # if not T5f.check_password():
     # st.stop()  # Do not continue if check_password is not True.
@@ -98,7 +101,7 @@ enddate = pd.to_datetime(enddate)
 df_new = df[(df['monthdt'] >= startdate) & (df['monthdt'] <= enddate) & (df.location.isin(options))]
 
 ext_melt = pd.melt(extra, 
-                     id_vars=['Location', 'location', 'Date'], 
+                     id_vars=['Location', 'location', 'Date'],   # others: Pmix_perc	Big5_perc	BayTimes
                      var_name='metric', 
                      value_name='value').dropna(subset=['value'])
 ext_melt = ext_melt[(ext_melt.location.isin(options))]
@@ -204,7 +207,158 @@ with c2:
 
 
 
-############################## Trend line test (linear)
+####### Create BOXES Showing comparison for previous month and vs. budget for ARO, CPD, LHPC,.... 
+box_height = 140
+
+row0 = st.columns(6)
+row1 = st.columns(6)
+
+# ARO	CPD  	LHPC	P-Mix %	  Big 5 %	Bay Times
+ind = [( 2, 'ARO'),( 1, 'CPD'),(51, 'LHPC'),(61, 'P-Mix %'),(62, 'Big 5 %'),(63, 'Bay Times')]
+
+last2mos = pivot_df.iloc[:,-5:-3].loc[ind,:]
+last2mos['diffs'] = last2mos.iloc[:,1].sub(last2mos.iloc[:,0], axis = 0) 
+last2mos['diffperc'] = last2mos['diffs'] / last2mos.iloc[:,0]
+last2mos = last2mos.reset_index().drop(columns=['Account_Num', 'Account'])
+last2mos.index = pd.RangeIndex(start=0, stop=len(last2mos), step=1)
+
+formatting = [
+    (0, dollar_form),
+    (1, format_two_decimals),
+    (2, format_two_decimals),
+    (3, pmix_form),
+    (4, big5_form),
+    (5, baytime_form),
+]
+for index, func in formatting:
+    last2mos.iloc[index, 1] = func(last2mos.iloc[index, 1])
+    # last2mos.loc[index, 'values'] = last2mos[index][2]
+cnt = 0
+for col in row0:
+    tile = col.container()#height=60)
+    tile.write(ind[cnt][1])
+    cnt += 1
+
+cnt = 0
+for col in row1:
+    tile = col.container(height=box_height)
+    tile.write(last2mos.iloc[cnt,1] + arrow_form_perc(last2mos.iloc[cnt]['diffperc']))
+    tile.write("All")
+    tile.write('(budget #s)')
+    cnt += 1
+
+ind = (df['monthdt'] >= enddate - pd.DateOffset(months=1)) & (df['monthdt'] <= enddate)
+df2months = df[ind]
+
+# calc ARO dataframe by location
+aro_df = df_rev / ext_cars_loc
+aro_df = aro_df.reset_index()
+ind = (aro_df['monthdt'] >= enddate - pd.DateOffset(months=1)) & (aro_df['monthdt'] <= enddate)
+aro_df = aro_df[ind].groupby(['location','monthdt'])['value'].mean().reset_index()
+aro_df = aro_df.pivot_table(index=['location'], columns='monthdt', values='value', aggfunc='mean')
+aro_df.columns = aro_df.columns.strftime('%b %y')
+aro_df['diffs'] = aro_df.iloc[:,1].sub(aro_df.iloc[:,0], axis = 0)
+aro_df['diffperc'] = aro_df['diffs'] / aro_df.iloc[:,0]
+
+### calculate CPD and LHPC for last month
+ind = (ext_sum['Date'] >= enddate - pd.DateOffset(months=1)) & (ext_sum['Date'] <= enddate)
+ext2_sum  = ext_sum.loc[ind,:]
+ext2_sum = ext2_sum.merge(workdays, left_on='Date', right_on='date')
+# get number of stores that are serving cars by month
+ind = (ext2_sum.metric == 'CarsServ')  
+n_stores_df = ext2_sum.loc[ind,:].Date.value_counts().reset_index()  # get number of stores open by month
+ext2_sum = ext2_sum.merge(n_stores_df, left_on='Date', right_on='Date')
+ext2_sum = ext2_sum.pivot_table(index=['location','Date','workdays','count'], columns = ['metric'], values='value', aggfunc='mean').reset_index()
+ext2_sum['CPD'] = (ext2_sum.CarsServ / ext2_sum.workdays) #/ ext2_sum['count']
+ext2_sum['LHPC'] = ext2_sum.EmpHours / ext2_sum.CarsServ 
+
+# st.write(ext2_sum) 
+### CPD df
+cpd_df = ext2_sum.pivot_table(index=['location'], columns='Date', values='CPD', aggfunc='mean')#.reset_index()
+cpd_df.columns = cpd_df.columns.strftime('%b %y')
+cpd_df['diffs'] = cpd_df.iloc[:,1].sub(cpd_df.iloc[:,0], axis = 0) 
+cpd_df['diffperc'] = cpd_df['diffs'] / cpd_df.iloc[:,0]
+### LHPC df
+lhpc_df = ext2_sum.pivot_table(index=['location'], columns='Date', values='CPD', aggfunc='mean')#.reset_index()
+lhpc_df.columns = lhpc_df.columns.strftime('%b %y')
+lhpc_df['diffs'] = lhpc_df.iloc[:,1].sub(lhpc_df.iloc[:,0], axis = 0) 
+lhpc_df['diffperc'] = lhpc_df['diffs'] / lhpc_df.iloc[:,0]
+
+## get Pmix, Big5%, Bay Times
+ind = (ext_avg['Date'] >= enddate - pd.DateOffset(months=1)) & (ext_avg['Date'] <= enddate)
+
+ext2_avg = ext_avg.loc[ind,:]
+# st.write("ext2:", ext2_avg)
+ext2_avg = ext2_avg.groupby(['location','metric','Date'])['value'].mean().reset_index()
+ext2_avg = ext2_avg.pivot_table(index=['location','metric'], columns='Date', values='value', aggfunc='mean')#.reset_index()
+ext2_avg.columns = ext2_avg.columns.strftime('%b %y')
+ext2_avg['diffs'] = ext2_avg.iloc[:,1].sub(ext2_avg.iloc[:,0], axis = 0) 
+ext2_avg['diffperc'] = ext2_avg['diffs'] / ext2_avg.iloc[:,0]
+
+# Sort by the second column
+sec_col = ext2_avg.columns[1]  # get the most recent month
+ext2_avg = ext2_avg.reset_index()
+aro_df = aro_df.sort_values(by=sec_col, ascending=False).reset_index()
+cpd_df = cpd_df.sort_values(by=sec_col, ascending=False).reset_index()
+lhpc_df = lhpc_df.sort_values(by=sec_col, ascending=True).reset_index()
+
+pmix_df = ext2_avg.loc[ext2_avg.metric == 'Pmix_perc'].sort_values(by=sec_col, ascending=False).reset_index()
+big5_df = ext2_avg.loc[ext2_avg.metric == 'Big5_perc'].sort_values(by=sec_col, ascending=False).reset_index()
+baytime_df = ext2_avg.loc[ext2_avg.metric == 'BayTimes'].sort_values(by=sec_col, ascending=True).reset_index()
+
+
+# Initialize a dictionary to store the containers
+grid = {}
+# # Define the number of rows and columns
+num_rows = len(options)
+num_cols = 6
+
+# # Create the grid of containers
+for row in range(num_rows):
+    grid[row] = st.columns(num_cols)
+#### ARO
+for row in range(num_rows):
+    tile = grid[row][0].container(height=box_height)
+    tile.write(dollar_form(aro_df.loc[row,sec_col]) + arrow_form_num(aro_df.iloc[row]['diffs']))
+    tile.write(aro_df.loc[row,'location'])
+    tile.write('(budget #s)')
+#### CPD
+for row in range(num_rows):
+    tile = grid[row][1].container(height=box_height)
+    tile.write(numb_form(cpd_df.loc[row,sec_col]) + arrow_form_num(cpd_df.iloc[row]['diffs']))
+    tile.write(cpd_df.loc[row,'location'])
+    tile.write('(budget #s)')
+#### LHPC
+for row in range(num_rows):
+    tile = grid[row][2].container(height=box_height)
+    tile.write(numb_form(lhpc_df.loc[row,sec_col]) + arrow_form_num(lhpc_df.iloc[row]['diffs']))
+    tile.write(lhpc_df.loc[row,'location'])
+    tile.write('(budget #s)')
+#### PMix %
+for row in range(num_rows):
+    tile = grid[row][3].container(height=box_height)
+    tile.write(pmix_form(pmix_df.loc[row,sec_col]) + arrow_form_perc(pmix_df.iloc[row]['diffs']))
+    tile.write(pmix_df.loc[row,'location'])
+    tile.write('(budget #s)')
+#### Big 5%
+for row in range(num_rows):
+    tile = grid[row][4].container(height=box_height)
+    tile.write(big5_form(big5_df.loc[row,sec_col]) + arrow_form_perc(big5_df.iloc[row]['diffs']))
+    tile.write(big5_df.loc[row,'location'])
+    tile.write('(budget #s)')
+#### Bay Times
+for row in range(num_rows):
+    tile = grid[row][5].container(height=box_height)
+    tile.write(baytime_form(baytime_df.loc[row,sec_col]) + arrow_form_num(baytime_df.iloc[row]['diffs']))
+    tile.write(baytime_df.loc[row,'location'])
+    tile.write('(budget #s)')
+
+
+
+
+### Test Area
+st.markdown("### Test Area - Future Improvements")
+############################# Trend line test (linear)
 # Prepare data for forecasting
 df_grouped = ext_cars_by_loc.groupby('Date').sum().reset_index()
 # Forecast for the next 3 months
@@ -230,131 +384,3 @@ fig2 = px.bar(ext_cars_by_loc, x='Date', y='value', color='location', title="Car
 fig2.add_scatter(x=combined_df['Date'], y=combined_df['value'], mode='lines', name='Trend Line')
 st.plotly_chart(fig2)
 ######################################################
-
-
-####### Create BOXES Showing comparison for previous month and vs. budget for ARO, CPD, LHPC,.... 
-
-row0 = st.columns(6)
-row1 = st.columns(6)
-row2 = st.columns(6)
-row3 = st.columns(6)
-row4 = st.columns(6)
-row5 = st.columns(6)
-row6 = st.columns(6)
-row7 = st.columns(6)
-
-# ARO	CPD  	LHPC	P-Mix %	  Big 5 %	Bay Times
-ind = [( 2, 'ARO'),( 1, 'CPD'),(51, 'LHPC'),(61, 'P-Mix %'),(62, 'Big 5 %'),(63, 'Bay Times')]
-
-last2mos = pivot_df.iloc[:,-5:-3].loc[ind,:]
-last2mos['diffs'] = last2mos.iloc[:,1].sub(last2mos.iloc[:,0], axis = 0) 
-last2mos['diffperc'] = last2mos['diffs'] / last2mos.iloc[:,0]
-last2mos = last2mos.reset_index().drop(columns=['Account_Num', 'Account'])
-last2mos.index = pd.RangeIndex(start=0, stop=len(last2mos), step=1)
-
-formatting = [
-    (0, dollar_format),
-    (1, dollar_format),
-    (2, format_two_decimals),
-    (3, perc_format),
-    (4, perc_format),
-    (5, format_two_decimals),
-]
-
-for index, func in formatting:
-    last2mos.iloc[index, 1] = func(last2mos.iloc[index, 1])
-    # last2mos.loc[index, 'values'] = last2mos[index][2]
-
-st.write(last2mos)
-cnt = 0
-for col in row0:
-    tile = col.container()#height=60)
-    tile.write(ind[cnt][1])
-    cnt += 1
-
-
-cnt = 0
-for col in row1:
-    tile = col.container(height=200)
-    tile.write(last2mos.iloc[cnt,1])
-    tile.write("All")
-    tile.write(arrow_format(last2mos.iloc[cnt]['diffperc']))
-    tile.write('(placeholder budget)')
-    cnt += 1
-
-ind = (df['monthdt'] >= enddate - pd.DateOffset(months=1)) & (df['monthdt'] <= enddate)
-df_loc = df_new[ind].groupby(['location','monthdt'])['value'].mean()#.reset_index()
-st.write(df_loc)
-    # + row2 + row3 + row4+ row5 + row6 + row7:
-    # tile = col.container(height=60)
-    # tile.title(":balloon:")
-
-
-# ind_sum = [(11, 'Revenue'),    (12, 'Gross Profit'), (25, '4-Wall EBITDA'), 
-#             (26, '4-Wall FCF'), (27, 'Net Profit'),   (71, '# of Cars Serviced')    ]
-# ind_avg = [( 1, 'CPD'),            ( 2, 'ARO'),              (21, 'Labor %'),
-#             (22, 'Controllable %'),(23, 'Uncontrollable %'), (31, 'Cash'),
-#             (41, 'Gross Profit %'),(42, '4-Wall EBITDA %'),  (43, '4-Wall FCF %'),
-#             (44, 'Net Profit %'),  (51, 'LHPC'),             (52, 'Revenue Per Employee Hours Worked'),
-#             (61, 'P-Mix %'),       (62, 'Big 5 %'),          (63, 'Bay Times'),
-#             (64, 'Discount %'),    (72, 'Gross Profit Per Car'), (73, '4-Wall EBITDA Per Car')]
-
-
-############################## FROM CHATGPT ##############################
-# # Assuming last2mos and ind are defined
-# # last2mos = pd.DataFrame({...})
-# # ind = [...]
-
-# # Define the number of rows and columns
-# num_rows = 8
-# num_cols = 6
-
-# # Initialize a dictionary to store the containers
-# grid = {}
-
-# # Create the grid of containers
-# for row in range(num_rows):
-#     grid[row] = st.columns(num_cols)
-
-# # Example data (you can replace this with your actual data)
-# last2mos = pd.DataFrame({
-#     'col1': ['Data11', 'Data12', 'Data13', 'Data14', 'Data15', 'Data16'],
-#     'diffperc': [10, 20, 30, 40, 50, 60]
-# })
-# ind = [['', 'Ind1'], ['', 'Ind2'], ['', 'Ind3'], ['', 'Ind4'], ['', 'Ind5'], ['', 'Ind6']]
-
-# # Fill the first row with ind data
-# for col in range(num_cols):
-#     tile = grid[0][col].container()
-#     if col < len(ind):
-#         tile.write(ind[col][1])
-
-# # Fill the second row with last2mos data and additional information
-# for col in range(num_cols):
-#     tile = grid[1][col].container()
-#     if col < len(last2mos):
-#         tile.write(last2mos.iloc[col, 1])
-#         tile.write("All")
-#         tile.write(f"DiffPerc: {last2mos.iloc[col]['diffperc']}%")
-#         tile.write('(placeholder budget)')
-
-# # You can continue to fill other rows in a similar manner
-
-# Explanation:
-# Data Initialization:
-
-# last2mos DataFrame is created with sample data. Replace this with your actual data.
-# ind list is defined with your provided values.
-# Grid Creation:
-
-# A dictionary grid is initialized to store the columns for each row.
-# Loop through the number of rows to create the columns using st.columns(num_cols).
-# Populating the Grid:
-
-# The first row (grid[0]) is filled with data from the ind list.
-# The second row (grid[1]) is filled with data from the last2mos DataFrame along with additional information.
-# Modular Approach:
-
-# The code is structured in a modular way, making it easy to add more rows and populate them with data as needed.
-# This code will create a grid of containers in Streamlit, with the first row displaying the ind values and the second row displaying data 
-# from the last2mos DataFrame. You can further extend this approach to fill other rows and columns with the desired data.
