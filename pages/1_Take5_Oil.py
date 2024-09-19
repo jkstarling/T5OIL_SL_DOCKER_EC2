6f456fb0c726ec0f157c6d6c2a90cfb7332a4bf5
jkstarling <beets09@yahoo.com>
2024-09-18 22:35:13 -0400

    update

--- pages/1_Take5_Oil.py
+++ pages/1_Take5_Oil.py
@@ -134,6 +134,7 @@ gauge_dict = {
 df1 = pivot_df.loc[(1,'CPD')].iloc[:-3]
 st.write(df1)
 st.write(df1.pct_change())
+df1 = df1.pct_change()
 min1, max1 = min(df1), max(df1)
 fig.add_trace(go.Indicator(
     title={'text': size + gauge_dict['titles'][0]},
