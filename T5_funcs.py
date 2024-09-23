d86d4e6f915a275ad03607c5e5a901686fd18f72
jkstarling <beets09@yahoo.com>
2024-09-22 22:06:17 -0400

    update

--- T5_funcs.py
+++ T5_funcs.py
@@ -81,7 +81,7 @@ def create_T5_pivot_table(result_df, ext_avg, ext_sum, controlmap, workdays, act
     pivot_table.loc[(1, 'CPD'),:] = np.round(ext_sum.loc['CarsServ',:] / work_pivot.loc['workdays',:], 1 )
     n_len = len(pivot_table.loc[(1, 'CPD'),:])
     st.write(pivot_table.loc[(1, 'CPD'), :], n_len)
-    st.write(actstores)
+    st.write(actstores['active'][-n_len:].values)
     # pivot_table.loc[(1, 'CPD'), :] = pivot_table.loc[(1, 'CPD'), :].div(ext2_sum['count'][-n_len:].values)
     pivot_table.loc[(1, 'CPD'), :] = pivot_table.loc[(1, 'CPD'), :].div([3,3,4,5,5,5,6,6,6,6,6,6]).round(0)
 
